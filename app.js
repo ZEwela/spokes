1050cffd1b0b89e6d2a6e2849e8375c33ea01926
Jason Ovenden <118778844+Ovenator27@users.noreply.github.com>
2024-03-22 15:44:08 +0000

    Create GET users endpoint and generic routing/server error handling

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const apiRouter = require("./routers/api.router");
+const { handleServerErrors } = require("./controllers/errors.controllers");
 const app = express();
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
+
+app.all("*", (req, res) => {
+    res.status(404).send({ msg: "Path not found" });
+  });
+
+app.use(handleServerErrors)
 
 module.exports = app;
