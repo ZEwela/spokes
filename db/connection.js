226849675c710e9f58576892d8b15dd2a5b4d570
Matt Jones <jockey@DESKTOP-T4USUPJ>
2024-03-26 16:35:00 +0000

    added hosting functionality

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,16 @@
+const ENV = process.env.NODE_ENV || 'development';
+
 const { Pool } = require("pg");
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE configured")
 }
 
-module.exports = new Pool();
+const config = {}
+
+if (ENV === 'production') {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+  }
+
+module.exports = new Pool(config);
