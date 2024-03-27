fe06a936bbd99b859a703e82225ff4143ecb0763
Ruby Faye Maria <rubyfayemaria@Rubys-MBP.broadband>
2024-03-27 09:58:58 +0000

    dotenv configuration

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,9 @@
 const ENV = process.env.NODE_ENV || 'development';
-
 const { Pool } = require("pg");
+
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE configured")
