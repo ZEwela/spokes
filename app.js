75ce8d3d2039594cea0e4051fba3f7fd752af756
Ewelina <ewelinazawol@gmail.com>
2024-03-27 16:42:31 +0000

    cors fix

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const { handleServerErrors, handlePsqlErrors, handleCustomErrors } = require("./
 const app = express();
 const cors = require('cors')
 
-app.use(express.json());
-
 app.use(cors())
+
+app.use(express.json());
 
 app.use("/api", apiRouter);
 
