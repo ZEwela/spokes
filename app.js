67c500efd070a5db57e9fc1ff25e4a5f8805f318
Ewelina <ewelinazawol@gmail.com>
2024-03-27 16:37:58 +0000

    cors setup

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const apiRouter = require("./routers/api.router");
 const { handleServerErrors, handlePsqlErrors, handleCustomErrors } = require("./controllers/errors.controllers");
 const app = express();
+const cors = require('cors')
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use("/api", apiRouter);
 
